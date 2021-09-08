eddbf4f392f0b250d990f03b919e79b671a69818
SalamanderKipp <200507@student.glu.nl>
2021-09-08 12:42:05 +0200

    labelsfixedandmore

--- loginpagina/src/App.js
+++ loginpagina/src/App.js
@@ -1,8 +1,11 @@
+import { useState } from 'react'
+import { useState, useEffect } from 'react'
+
 function App() {
   return (
-    <Dummy text="Log in bij naam">
-      <FormInput type="text" placeholder="name"/>
-      <FormInput type="password" placeholder="password"/>
+    <Dummy text="Log in bij SalamanderKip.nl">
+      <FormInput type="text" placeholder="username" class="tooltip" id="username" labelname="username"/>
+      <FormInput type="password" placeholder="password" class="tooltip" id="password" labelname="password"/>
     </Dummy>
   );
 }
@@ -12,13 +15,18 @@ function Dummy(props) {
     <div className='main'>
     <h1>{ props.text }</h1>
       { props.children }
-    <a>Inloggen</a>
+    <a className="login-button" href="#">Inloggen</a>
     </div>
   );
 }
+
 function FormInput(props) {
   return (
-    <input type={ props.type } placeholder={ props.placeholder }></input> 
+    <div class="input-wrapper">
+      <input type={ props.type } placeholder={ props.placeholder } id={ props.id }></input>
+      <label for={ props.id }>{ props.labelname }</label> 
+      <span class="tooltiptext"> </span>
+    </div>
   );
 }
 export default App;
